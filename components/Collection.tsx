42bdccfd8a282b1f041ec8439ac5587801ae1db7
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2022-10-25 12:29:30 -0400

    Display collection and let brand's user choose which collection to mint

---
+++ components/Collection.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image"
+import { useRouter } from "next/router"
+import NFTImage from "../public/nft-image.png"
+
+function Collection({price}) {
+
+    const router = useRouter()
+
+    async function mintNft() {
+
+       // fetch collections available
+    
+        const res = await fetch(`https://staging.crossmint.io/api/v1-alpha1/checkout/mint`, {
+            method: 'POST',
+            body: JSON.stringify({
+                clientId: "e844f3de-e3f9-4fcb-abc6-15e1bff5e6d7",
+                userId: "6346b401fdf86137e454caac",
+                emailTo: "nhi@paella.dev",
+                mintTo: "D6bDg4DcCpsprQEmnUdcWhpkj2pMDxEJa615oyvS3sQL",
+                paymentMethod: "fiat",
+                mintConfig: {
+                    type: "candy-machine",
+                    totalPrice: "0.0001",
+                    environment: "staging"
+                },
+                collection: {
+                    title: "whitelabel update collection test"
+                }
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+              'X-PROJECT-ID': process.env.CROSSMINT_X_PROJECT_ID,
+              'X-CLIENT-SECRET': process.env.CROSSMINT_X_CLIENT_SECRET
+            }
+        })
+        const data = await res.json()
+        router.push(data.checkoutUrl)
+    }
+
+    return (
+        <div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <Image className="p-8 rounded-t-lg" src={NFTImage} alt="product image"/>
+            </a>
+            <div className="px-5 pb-5">
+                <a href="#">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Collection Title</h5>
+                </a>
+                <div className="flex items-center mt-2.5 mb-5">
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+                </div>
+                <div className="flex justify-between items-center">
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                    <button onClick={mintNft} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Mint this collection</button>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Collection
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,44 +1,63 @@
-function CheckoutPage({ data }) {
+import Collection from "../components/Collection"
+
+function CheckoutPage({  }) {
     // Render data...
     return (
-        <div  className="flex h-screen justify-center items-center">
-            <a href={data.checkoutUrl}>
+        <div  className="flex-col w-full h-full justify-center items-center">
+            <h1 className="text-3xl font-bold">Available Collections</h1>
+            <div className="flex overflow-x-scroll hover:overflow-scroll space-x-12 bg-gray-200 p-10">
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+                <Collection title={"Collection Name"} price={"0.0001"} />
+            </div>
+            {/* <a href={data.checkoutUrl}>
                 <button className="text-xl bg-transparent hover:bg-green-500 text-green-700 font-bold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded">Checkout</button>
-            </a>
+            </a> */}
         </div>
     )
   }
 
-export async function getServerSideProps() {
 
-    const res = await fetch(`https://staging.crossmint.io/api/v1-alpha1/checkout/mint`, {
-        method: 'POST',
-        body: JSON.stringify({
-            clientId: "e844f3de-e3f9-4fcb-abc6-15e1bff5e6d7",
-            userId: "6346b401fdf86137e454caac",
-            emailTo: "nhi@paella.dev",
-            mintTo: "D6bDg4DcCpsprQEmnUdcWhpkj2pMDxEJa615oyvS3sQL",
-            paymentMethod: "fiat",
-            mintConfig: {
-                type: "candy-machine",
-                totalPrice: "0.0001",
-                environment: "staging"
-            },
-            collection: {
-                title: "whitelabel update collection test"
-            }
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-          'X-PROJECT-ID': process.env.CROSSMINT_X_PROJECT_ID,
-          'X-CLIENT-SECRET': process.env.CROSSMINT_X_CLIENT_SECRET
-        }
-    })
-    const data = await res.json()
 
-    return {
-      props: { data }, // will be passed to the CheckoutPage component as props
-    }
-}
+// export async function getServerSideProps() {
+
+    // fetch collections available
+
+    // const res = await fetch(`https://staging.crossmint.io/api/v1-alpha1/checkout/mint`, {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         clientId: "e844f3de-e3f9-4fcb-abc6-15e1bff5e6d7",
+    //         userId: "6346b401fdf86137e454caac",
+    //         emailTo: "nhi@paella.dev",
+    //         mintTo: "D6bDg4DcCpsprQEmnUdcWhpkj2pMDxEJa615oyvS3sQL",
+    //         paymentMethod: "fiat",
+    //         mintConfig: {
+    //             type: "candy-machine",
+    //             totalPrice: "0.0001",
+    //             environment: "staging"
+    //         },
+    //         collection: {
+    //             title: "whitelabel update collection test"
+    //         }
+    //     }),
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'X-PROJECT-ID': process.env.CROSSMINT_X_PROJECT_ID,
+    //       'X-CLIENT-SECRET': process.env.CROSSMINT_X_CLIENT_SECRET
+    //     }
+    // })
+    // const data = await res.json()
+
+    // return {
+    //   props: { data }, // will be passed to the CheckoutPage component as props
+    // }
+//}
 
 export default CheckoutPage
