d6b54ada01726e6001b715b7da166c6c9071a103
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2022-10-29 10:31:24 -0400

    redirect to login page

--- pages/flow/1.tsx
+++ pages/flow/1.tsx
@@ -56,7 +56,7 @@ function Page() {
                 </div>
             </nav>
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-                <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
+                <a href="/flow/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
                 <a href="#" className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
             </div>
             </div>
