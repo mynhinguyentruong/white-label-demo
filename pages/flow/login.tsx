029408cac4cc1381923bb9166c97fe2d93ab891c
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2022-10-29 10:43:45 -0400

    track email state in login

--- pages/flow/login.tsx
+++ pages/flow/login.tsx
@@ -1,7 +1,15 @@
 import CrossmintLogo from "../../public/crossmint-logo.svg"
 import Image from "next/image"
+import { useState } from "react"
 
 function Page() {
+
+    const [email, setEmail] = useState("")
+
+    function login(e) {
+        e.preventDefault()
+        console.log(email)
+    }
     return (
         <>
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -21,7 +29,7 @@ function Page() {
               Bring your own auth to sign your user in
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" onSubmit={login}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
@@ -36,18 +44,16 @@ function Page() {
                   required
                   className="h-10 relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Email address"
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               
             </div>
-
-            
-
             <div>
             <a href="/flow/1">
               <button
                 type="button"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Sign in
               </button>
