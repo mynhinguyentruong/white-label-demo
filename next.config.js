1bf1c3ca6310189b08a22cdd1418b1eba93c31ee
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2022-10-29 14:58:12 -0400

    Flow collection

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['www.crossmint.io', 'nftstorage.link']
+    domains: ['www.crossmint.io', 'nftstorage.link', 'tailwindui.com']
   },
   async rewrites() {
     return [
--- pages/flow/collections.tsx
+++ pages/flow/collections.tsx
@@ -1,16 +1,56 @@
 import Layout from "../../components/Layout"
+import Image from "next/image"
+
+import CollectionImage from "../../public/collection-image.png"
+
+import TigerOne from "../../public/tiger-2.png"
+import TigerTwo from "../../public/tiger-3.png"
+import TigerThree from "../../public/tiger-4.png"
 
 function Page() {
     return (
         <>
         <Layout/>
-        <div className="relative mx-auto mt-10 max-w-7xl px-4 sm:px-6">
-               <h1 className="font-bold text-5xl my-3">
-                    No Collections Found
-               </h1>
-               <p>We couldn't find anything in your collection</p>
-               <p>Visit <a href="/flow/collections" className="hover:text-green-700 text-green-400">Collections </a> to buy NFT on our site</p>
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                <h2 className="sr-only">Products</h2>
+                
+                <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                    <a href="#" className="group">
+                        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                            <Image width={280} height={320} src={CollectionImage} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+                        </div>
+                        <h3 className="mt-4 text-sm text-gray-700">Tiger One</h3>
+                        <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
+                    </a>
+
+                    <a href="#" className="group">
+                        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                        <Image width={280} height={320} src={TigerOne} alt="Olive drab green insulated bottle with flared screw lid and flat top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+                        </div>
+                        <h3 className="mt-4 text-sm text-gray-700">Tiger Two</h3>
+                        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+                    </a>
+
+                    <a href="#" className="group">
+                        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                        <Image width={280} height={320} src={TigerTwo} alt="Person using a pen to cross a task off a productivity paper card." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+                        </div>
+                        <h3 className="mt-4 text-sm text-gray-700">Tiger Three</h3>
+                        <p className="mt-1 text-lg font-medium text-gray-900">$89</p>
+                    </a>
+
+                    <a href="#" className="group">
+                        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                        <Image width={280} height={320} src={TigerThree} alt="Hand holding black machined steel mechanical pencil with brass tip and top." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+                        </div>
+                        <h3 className="mt-4 text-sm text-gray-700">Tiger Three</h3>
+                        <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
+                    </a>
+
+                </div>
             </div>
+        </div>
         </>
     )
 }
