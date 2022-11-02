92aac3f7f65bb1a134ca5f7b69fd8702df70cd64
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2022-11-02 14:55:07 -0400

    Update wallets.tsx

--- pages/flow/wallets.tsx
+++ pages/flow/wallets.tsx
@@ -10,7 +10,7 @@ function Page({data}) { // data is an array
             <Layout/>
             {data.length > 0 ? (
                 <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <h2 className="font-bold mb-10 text-3xl pb-10">My NFTs</h2>
+                    <h2 className="font-bold mb-10 text-3xl pb-10">Buy NFTs</h2>
                 
                     <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                         {data.map(nft => (
