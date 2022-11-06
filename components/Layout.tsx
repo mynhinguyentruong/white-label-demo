948599ee44a28634e4d2bb28e4bc3da0e40a5d09
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2022-11-06 09:53:36 -0500

    Remove svg

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,9 +23,7 @@ function Layout() {
                             <button type="button" className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                             <span className="sr-only">Open menu</span>
                             {/* <!-- Heroicon name: outline/bars-3 --> */}
-                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
+                            
                             </button>
                         </div>
             <nav className="hidden space-x-10 md:flex">
