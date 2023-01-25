e9c4ffbeb817086c546a90f22cd99ec02c0aae01
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-01-25 14:29:51 +0700

    remove exposure of env to the client (risky)

--- next.config.js
+++ next.config.js
@@ -25,12 +25,12 @@ const nextConfig = {
       },
     ]
   },
-  env: {
-    CROSSMINT_X_CLIENT_SECRET: process.env.CROSSMINT_X_CLIENT_SECRET,
-    CROSSMINT_X_PROJECT_ID: process.env.CROSSMINT_X_PROJECT_ID,
-    API_KEY: process.env.API_KEY,
-    PROD_PROJECT_ID: process.env.PROD_PROJECT_ID
-  }
+  // env: {
+  //   CROSSMINT_X_CLIENT_SECRET: process.env.CROSSMINT_X_CLIENT_SECRET,
+  //   CROSSMINT_X_PROJECT_ID: process.env.CROSSMINT_X_PROJECT_ID,
+  //   API_KEY: process.env.API_KEY,
+  //   PROD_PROJECT_ID: process.env.PROD_PROJECT_ID
+  // }
 }
 
 module.exports = nextConfig
